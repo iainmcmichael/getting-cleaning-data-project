7b098db15170b2c9b3671966af317a470ecb711f
Reiners, Paul <Paul.Reiners@Pearson.com>
2014-06-13 17:04:08 -0500

    Work, work, work.

--- src/run_analysis.R
+++ src/run_analysis.R
@@ -1,14 +1,18 @@
 # Merge the training and the test sets to create one data set.
-features <- read.table("./data/features.txt", sep = "" , header = F, na.strings ="", stringsAsFactors = F)
+features <- read.table("./data/features.txt", na.strings ="", stringsAsFactors = F)
 colnames(features) <- c("id", "name")
 
 X_train <- read.table("./data/train/X_train.txt", na.strings = "", stringsAsFactors = F)
-X_train <- X_train[complete.cases(X_train),]
 colnames(X_train) <- features$name
+subject_train <- read.table("./data/train/subject_train.txt", na.strings = "", stringsAsFactors = F)
+colnames(subject_train) = ("subject.id")
+X_train$subject.id <- subject_train$subject.id
 
 X_test <- read.table("./data/test/X_test.txt", na.strings = "", stringsAsFactors = F)
-X_test <- X_test[complete.cases(X_test),]
 colnames(X_test) <- features$name
+subject_test <- read.table("./data/test/subject_test.txt", na.strings = "", stringsAsFactors = F)
+colnames(subject_test) = ("subject.id")
+X_test$subject.id <- subject_test$subject.id
 
 all.data <- rbind(X_train, X_test)
 
@@ -16,3 +20,4 @@ all.data <- rbind(X_train, X_test)
 # Uses descriptive activity names to name the activities in the data set
 # Appropriately labels the data set with descriptive variable names. 
 # Creates a second, independent tidy data set with the average of each variable for each activity and each subject. 
+cdata <- aggregate(. ~ subject.id, data = all.data, FUN = function(x) c(mean = mean(x), sd = sd(x)))
