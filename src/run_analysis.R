b38d92c74aaae4fb108c0d5d7be39f90abcdaf5b
Paul Reiners <paul.reiners@gmail.com>
2014-06-15 14:57:30 -0500

    Finishing up...

--- src/run_analysis.R
+++ src/run_analysis.R
@@ -3,31 +3,44 @@
 ## Getting and Cleaning Data
 ## Course Project
 
+# Download original data
+url <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
+zipfile <- "Dataset.zip"
+download.file(url, zipfile, "curl")
+unzip(zipfile)
+file.remove(zipfile)
+data.dir <- "./UCI HAR Dataset/"
+
 # 4. Appropriately label the data set with descriptive variable names. 
-features <- read.table("./data/features.txt", na.strings ="", stringsAsFactors = F)
+features <- read.table(paste(data.dir, "features.txt", sep=""), na.strings ="", stringsAsFactors = F)
 colnames(features) <- c("id", "name")
 columnNames <- features$name
 
 # 1. Merge the training and the test sets to create one data set.
-train <- read.table("./data/train/subject_train.txt", na.strings = "", stringsAsFactors = F)
+train.dir <- paste(data.dir, "train/", sep="")
+train <- read.table(paste(train.dir, "subject_train.txt", sep=""), na.strings = "", stringsAsFactors = F)
 colnames(train) = ("subject")
-Y_train <- read.table("./data/train/Y_train.txt", na.strings = "", stringsAsFactors = F)
+Y_train <- read.table(paste(train.dir, "Y_train.txt", sep=""), na.strings = "", stringsAsFactors = F)
 colnames(Y_train) <- c("activity")
 train <- cbind(train, Y_train)
-X_train <- read.table("./data/train/X_train.txt", na.strings = "", stringsAsFactors = F)
+X_train <- read.table(paste(train.dir, "X_train.txt", sep=""), na.strings = "", stringsAsFactors = F)
 colnames(X_train) <- columnNames
 train <- cbind(train, X_train)
 
-test <- read.table("./data/test/subject_test.txt", na.strings = "", stringsAsFactors = F)
+test.dir <- paste(data.dir, "test/", sep="")
+test <- read.table(paste(test.dir, "subject_test.txt", sep=""), na.strings = "", stringsAsFactors = F)
 colnames(test) = ("subject")
-Y_test <- read.table("./data/test/Y_test.txt", na.strings = "", stringsAsFactors = F)
+Y_test <- read.table(paste(test.dir, "Y_test.txt", sep=""), na.strings = "", stringsAsFactors = F)
 colnames(Y_test) <- c("activity")
 test <- cbind(test, Y_test)
-X_test <- read.table("./data/test/X_test.txt", na.strings = "", stringsAsFactors = F)
+X_test <- read.table(paste(test.dir, "X_test.txt", sep=""), na.strings = "", stringsAsFactors = F)
 colnames(X_test) <- columnNames
 test <- cbind(test, X_test)
 
 all.data <- rbind(train, test)
+
+# Clean up original data
+unlink(data.dir, recursive = TRUE)
 
 # 2. Extract only the measurements on the mean and standard deviation for each 
 #    measurement. 
@@ -47,3 +60,6 @@ all.data$activity <- as.factor(all.data$activity)
 # 5. Create a second, independent tidy data set with the average of each 
 #    variable for each activity and each subject. 
 tidy.data <- aggregate(. ~ subject + activity, data = all.data, FUN = mean)
+
+# Write data
+write.table(tidy.data, "TidyData.txt")
