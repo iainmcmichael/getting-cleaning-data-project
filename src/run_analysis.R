fa2860a18036a7eff4db56ac54f043e3e412440f
Paul Reiners <paul.reiners@gmail.com>
2014-06-15 12:15:23 -0500

    Adding activity column.

--- src/run_analysis.R
+++ src/run_analysis.R
@@ -10,12 +10,18 @@ columnNames <- features$name
 
 train <- read.table("./data/train/subject_train.txt", na.strings = "", stringsAsFactors = F)
 colnames(train) = ("subject")
+Y_train <- read.table("./data/train/Y_train.txt", na.strings = "", stringsAsFactors = F)
+colnames(Y_train) <- c("activity")
+train <- cbind(train, Y_train)
 X_train <- read.table("./data/train/X_train.txt", na.strings = "", stringsAsFactors = F)
 colnames(X_train) <- columnNames
 train <- cbind(train, X_train)
 
 test <- read.table("./data/test/subject_test.txt", na.strings = "", stringsAsFactors = F)
 colnames(test) = ("subject")
+Y_test <- read.table("./data/test/Y_test.txt", na.strings = "", stringsAsFactors = F)
+colnames(Y_test) <- c("activity")
+test <- cbind(test, Y_test)
 X_test <- read.table("./data/test/X_test.txt", na.strings = "", stringsAsFactors = F)
 colnames(X_test) <- columnNames
 test <- cbind(test, X_test)
@@ -24,7 +30,7 @@ all.data <- rbind(train, test)
 
 # 2. Extract only the measurements on the mean and standard deviation for each measurement. 
 is.mean.or.std <- grepl("-(mean|std)\\(\\)", columnNames, ignore.case = TRUE)
-all.data <- all.data[, c(TRUE, is.mean.or.std)]
+all.data <- all.data[, c(TRUE, TRUE, is.mean.or.std)]
 
 # 3. Uses descriptive activity names to name the activities in the data set
 # 4. Appropriately labels the data set with descriptive variable names. 
